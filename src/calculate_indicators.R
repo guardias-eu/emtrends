6be657533906c26d482124d3aabdd9d5e86bb95f
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-04-02 10:29:06 +0200

    Improve indentation

--- src/calculate_indicators.R
+++ src/calculate_indicators.R
@@ -142,16 +142,16 @@ plot_indicators <- function(df, species_key) {
   if (nrow(df) == 0) return(NULL)
   else {
     plot_output <- df %>%
-    ggplot(aes(x = year, y = value)) +
-      geom_point() +
-    facet_wrap(~indicator, scales = "free_y", ncol = 1) +
-    labs(
-      title = paste("Species key:", species_key, ". n_occurrences (top), n_grid_cells (bottom)"),
-      x = "Year",
-      y = "Value"
-    ) +
-    theme_minimal()
     if (nrow(df) > 0) {
+      ggplot(aes(x = year, y = value)) +
+        geom_point() +
+        facet_wrap(~indicator, scales = "free_y", ncol = 1) +
+        labs(
+          title = paste("Species key:", species_key, ". n_occurrences (top), n_grid_cells (bottom)"),
+          x = "Year",
+          y = "Value"
+        ) +
+        theme_minimal()
       # Add lines to the plot only if there are more than 1 point. Otherwise, it will give a warning.
       return(plot_output + geom_line())
     } else {
